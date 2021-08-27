8e0777587302a92f03150e9305492a6f818b6a0c
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-08-27 15:42:39 +0100

    Add top ten movie array list

--- index.js
+++ index.js
@@ -4,10 +4,74 @@ const express = require("express"),
 const app = express();
 
 app.use(morgan("common"));
+
 app.use(express.static("public"));
 
+let topMovies = [
+  {
+    id: "1",
+    title: "The Notebook",
+    year: "2004",
+    genre: "Romance"
+  },
+  {
+    id: "2",
+    title: "A Star Is Born",
+    year: "2018",
+    genre: "Romance"
+  },
+  {
+    id: "3",
+    title: "Pride And Prejudice",
+    year: "2004",
+    genre: "Romance"
+  },
+  {
+    id: "4",
+    title: "The Sound Of Music",
+    year: "1965",
+    genre: "Romance"
+  },
+  {
+    id: "5",
+    title: "Get Out",
+    year: "2017",
+    genre: "Horror"
+  },
+  {
+    id: "6",
+    title: "Behind Her Eyes",
+    year: "2021",
+    genre: "Psychological"
+  },
+  {
+    id: "7",
+    title: "Mad Max Fury Road",
+    year: "2015",
+    genre: "Post-apocalypse/Fantasy"
+  },
+  {
+    id: "8",
+    title: "Avengers: Endgame",
+    year: "2019",
+    genre: "Action"
+  },
+  {
+    id: "9",
+    title: "Pretty Woman",
+    year: "2004",
+    genre: "Romance"
+  },
+  {
+    id: "10",
+    title: "A Quiet place",
+    year: "2018",
+    genre: "Horror"
+  }
+];
+
 app.get("/movies", (req, res) => {
-  res.json(topTenMovies);
+  res.json(topMovies);
 });
 
 app.get("/", (req, res) => {
@@ -19,6 +83,6 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-app.listen(8081, () => {
-  console.log("Your app is listening on port 8081.");
+app.listen(8083, () => {
+  console.log("Your app is listening on port 8083.");
 });
