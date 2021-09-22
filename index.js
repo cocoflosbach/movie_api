a38b5914228bd231b2bb37c81d602dabe24a3b5a
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-09-22 22:59:58 +0100

    Allow port to change as required

--- index.js
+++ index.js
@@ -424,6 +424,11 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-app.listen(8082, () => {
+/*app.listen(8082, () => {
   console.log("Your app is listening on port 8082.");
+});*/
+
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log("listening on Port " + port);
 });
