7c053f4cedab9eed54d5f88093bff142d9ee3d1c
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2021-09-23 14:56:30 +0100

    Connect API to online database

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const Users = Models.User;
 const Directors = Models.Director;
 const Genre = Models.Genre;
 
-mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+/*mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});*/
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
